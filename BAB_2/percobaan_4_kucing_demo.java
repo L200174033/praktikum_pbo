e2e9301bb99da9669185915f45be9b267508b437
Herda Pundhi Saputra <43403967+L200174033@users.noreply.github.com>
2018-10-03 09:22:38 -0400

    Update percobaan_4_kucing_demo.java

--- BAB_2/percobaan_4_kucing_demo.java
+++ BAB_2/percobaan_4_kucing_demo.java
@@ -1,7 +1,4 @@
-/*
- * @author Ilham Wicaksono
- * @app : netbeans IDE 8.2
- */
+
  public class KucingkuDemo {
     public static void main(String[] args){
         Kucingku Kucing = new Kucingku();
